e6599d6770a0bbd874e657ec59dd00f00cb45817
Korneliia <korneliiaPositive@gmail.com>
2024-02-15 18:30:14 +0100

    add styles

---
+++ src/Nav/Nav.jsx
@@ -0,0 +1,10 @@
+import style from "./Nav.module.css";
+
+const Nav = () => {
+    return (
+        <div className={style.container}>
+            Nav
+        </div>
+    )
+}
+export default Nav;
