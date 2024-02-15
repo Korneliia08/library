fc7b1619724ecee46b91c2cd95936ba3e452f9bd
Korneliia <korneliiaPositive@gmail.com>
2024-02-15 19:55:51 +0100

    add styles

---
+++ src/defaultPanel/staticComponents/ContainerForArticlesAndAside/Aside/LinkToBooks/LinkToBooks.jsx
@@ -0,0 +1,10 @@
+import style from "./LinkToBooks.module.css";
+
+const LinkToBooks = () => {
+    return (
+        <div className={style.container}>
+            LinkToBooks
+        </div>
+    )
+}
+export default LinkToBooks;
---
+++ src/defaultPanel/staticComponents/ContainerForArticlesAndAside/Aside/OldImage/OldImage.jsx
@@ -0,0 +1,10 @@
+import style from "./OldImage.module.css";
+
+const OldImage = () => {
+    return (
+        <div className={style.container}>
+            OldImage
+        </div>
+    )
+}
+export default OldImage;
