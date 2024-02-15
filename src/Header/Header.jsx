3a39edbebb4794225576d8cbaca6009884702328
Korneliia <korneliiaPositive@gmail.com>
2024-02-15 15:15:23 +0100

    create a project of library

---
+++ src/Header/Header.jsx
@@ -0,0 +1,10 @@
+import style from "./Header.module.css";
+
+const Header = () => {
+    return (
+        <div className={style.container}>
+            Header
+        </div>
+    )
+}
+export default Header;
---
+++ template/TemplateName.jsx
@@ -0,0 +1,10 @@
+import style from "./TemplateName.module.css";
+
+const TemplateName = () => {
+    return (
+        <div className={style.container}>
+            TemplateName
+        </div>
+    )
+}
+export default TemplateName;
