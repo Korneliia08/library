c581a9438283aae4763b2a06e49a4312a2379a4f
Korneliia <korneliiaPositive@gmail.com>
2024-02-16 16:50:48 +0100

    add styles

--- src/defaultPanel/staticComponents/ContainerForBlockOfLinksToSites/BlockOfLinksToSites/MainBlock/MainBlock.jsx
+++ src/defaultPanel/staticComponents/ContainerForBlockOfLinksToSites/BlockOfLinksToSites/MainBlock/MainBlock.jsx
@@ -1,9 +1,22 @@
 import style from "./MainBlock.module.css";
+import line from '../../../../assets/images/line.png'
 
 const MainBlock = () => {
     return (
         <div className={style.container}>
-            MainBlock
+            <img src={line} className={style.line}/>
+            <h4 className="titleOfHeading">Links to sites:</h4>
+            <span className={style.title}>biuletyn informacji publicznej</span>
+            <span className={style.title}>biblioteka narodowa</span>
+            <span className={style.title}>narodowy program rozwoju czytelnictwa</span>
+            <span className={style.title}>ministerstwo kultury i dziedzictwa narodowego</span>
+            <span className={style.title}>podkarpacka biblioteka cyfrowa</span>
+            <span className={style.title}>biblioteka portal inny niż wszystkie</span>
+            <span className={style.title}>instytut książki</span>
+            <span className={style.title}>działaj lojalnie</span>
+            <span className={style.title}>strona archiwum 1</span>
+            <span className={style.title}>strona archiwum 2</span>
+            <span className={style.title}>kab</span>
         </div>
     )
 }
