8be3b5e569b16a8b82427e0f5d47c2194ce6a1df
Korneliia <korneliiaPositive@gmail.com>
2024-02-16 20:14:14 +0100

    made a fixed nav

---
+++ src/defaultPanel/dynamicSubPages/ComponentWhichDesplayAll/ComponentWhichDesplayAll.jsx
@@ -0,0 +1,10 @@
+import style from "./ComponentWhichDesplayAll.module.css";
+
+const ComponentWhichDesplayAll = () => {
+    return (
+        <div className={style.container}>
+            ComponentWhichDesplayAll
+        </div>
+    )
+}
+export default ComponentWhichDesplayAll;
